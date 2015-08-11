0927859527ca27ad3135d6c11fa382f71c998098
SharpWave <sullivan.dw@gmail.com>
2015-08-11 11:10:10 -0400

    whatever the last thing was before my trip

--- RegCellAnalysis.m
+++ RegCellAnalysis.m
@@ -1,7 +1,7 @@
-function [bcounts,bcenters] = RegCellAnalysis(sessnums)
+function [bcounts,bcenters,TRates] = RegCellAnalysis(sessnums)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
+close all;
 cd('C:\MasterData');
 load MasterDirectory;
 cd(MD(sessnums(1)).Location);
@@ -9,8 +9,8 @@ load Reg_NeuronIDs.mat;
 
 nmap = Reg_NeuronIDs(1).all_session_map(:,2:end);
 
-NumNeurons = size(nmap,1);
-NumSessions = size(nmap,2);
+NumNeurons = size(nmap,1)
+NumSessions = size(nmap,2)
 
 for i = 1:length(sessnums)
     cd(MD(sessnums(i)).Location);
@@ -51,14 +51,21 @@ for i = 1:size(nmap,1)
 
 end
 figure;
-[bcounts,bcenters] = hist(NumSessActive,0:max(NumSessActive));
+[bcounts,bcenters] = hist(NumSessActive,0:max(NumSessActive));hold on;
+plot(bcenters(2:end),bcounts(2:end));
 xlabel('Number of sessions active');ylabel('Number of neurons');set(gca,'XLim',[0.5 max(NumSessActive)]);
 
 for i = 1:size(nmap,1)
 mt(i) = mean(TRates{i});
+
+end
+
+for i = 1:max(NumSessActive)
+    temp = find(NumSessActive == i);
+mmt(i) = mean(mt(temp));
 end
 
 figure;
-plot(NumSessActive,mt,'*');
-
-keyboard
+plot(mt,NumSessActive,'*');hold on;plot(mmt,1:max(NumSessActive),'-r');
+[rval,pval] = corr(mt',NumSessActive')
+keyboard;
