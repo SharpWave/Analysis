9ee77cbd3c222f6f0d49ff2041787ea70e84ee1f
SharpWave <sullivan.dw@gmail.com>
2016-01-25 12:42:43 -0500

    aligns the FT and ICFT to position tracking and truncates

--- Analyze_TvP.m
+++ Analyze_TvP.m
@@ -3,7 +3,13 @@ function [ output_args ] = Analyze_TvP(matfile)
 %   Detailed explanation goes here
 load(matfile);
 
-% load motion-ADJUSTED FT and ICFT
+% load motion-ADJUSTED FT and ICFT - replicate the truncation procedures in
+% CalculatePlacefields
+[~,~,~,FT,~,~,~,fStart] = AlignImagingToTracking(0.1,FT);
+[~,~,~,ICFT,~,~,~,fStart] = AlignImagingToTracking(0.1,ICFT);
+
+
+
 
 for i = 1:length(ICimage)
     ICpixels{i} = find(ICimage{i});
@@ -15,11 +21,23 @@ for i = 1:length(NeuronImage)
 end
 
 for i = 1:length(NeuronImage)
-    Num_T_Transients(i) = 
-    Num_ClosestI_Transients(i) = 
-    Num_Matching_Transients(i) = 
-    Fraction_T_Matched(i) = Num_T_Transients(i)./Num_Matching_Transients(i);
-    Fraction_ClosestI_Matched(i) = Num_ClosestI_Transients(i)./Num_Matching_Transients(i);
+    cidx = ClosestT(i);
+    
+    tEpochs = NP_FindSupraThresholdEpochs(FT(i,:),eps);
+    Num_T_Transients(i) = size(tEpochs,1);
+    
+    iEpochs = NP_FindSupraThresholdEpochs(ICFT(i,:),eps);
+    Num_ClosestI_Transients(i) = size(iEpochs,1);
+    
+    Num_Matching_Transients(i) = 0;
+    for j = 1:Num_T_Transients(i)
+      if (sum(ICFT(cidx,tEpochs(j,1):tEpochs(j,2))) > 0)
+         Num_Matching_Transients(i) = Num_Matching_Transients(i) + 1;
+      end
+    end
+        
+    Fraction_T_Matched(i) = Num_Matching_Transients(i)./Num_T_Transients(i);
+    Fraction_ClosestI_Matched(i) = Num_Matching_Transients(i)./Num_ClosestI_Transients(i);
 end
 
 keyboard;
