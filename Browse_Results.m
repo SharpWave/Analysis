995b73b98ffe1f360248f557db62f47e012c7d6d
SharpWave <sullivan.dw@gmail.com>
2016-06-18 19:47:35 -0400

    changes to browse scripts

--- Browse_Results.m
+++ Browse_Results.m
@@ -1,4 +1,4 @@
-function [ output_args ] = Browse_TvP()
+function [ output_args ] = Browse_Results()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 load('ProcOut.mat','FT');
@@ -7,11 +7,8 @@ load TvP_analysis.mat;
 load NormTraces.mat;
 load CorrTrace.mat;
 load ROIavg.mat;
+load T2output.mat;
 close all;
-
-
-
-
 
 for i = 1:length(NeuronImage)
     ROIgroup(i)
@@ -36,35 +33,38 @@ for i = 1:length(NeuronImage)
     caxis([minc maxc]);colorbar;hold off;
     %keyboard;
     
-    subplot(4,3,2);imagesc(MeanI{idx});axis equal;axis tight;
+    subplot(4,3,2);imagesc(MeanT{i});axis equal;axis tight;
     hold on;
-    plot(outI{i}{1}(:,2),outI{i}{1}(:,1),'-k');
-    title(['PCAICA Neuron #',int2str(idx),' frames = ',int2str(nAFI(idx))],'FontSize',8);
+    plot(outT{i}{1}(:,2),outT{i}{1}(:,1),'-r');
+    for j = 1:length(NeuronImage)
+        plot(outT{j}{1}(:,2),outT{j}{1}(:,1),'-g');
+    end
+
     set(gca,'XLim',[Tcent(i,1)-40 Tcent(i,1)+40]);
     set(gca,'YLim',[Tcent(i,2)-40 Tcent(i,2)+40]);
-    minc = 0;
-    maxc = max(MeanI{idx}(NeuronPixels{i}));
+%     minc = 0;
+%     maxc = max(MeanT{idx}(NeuronPixels{i}));
     set(gca,'XLim',[Xcent-40 Xcent+40]);
     set(gca,'YLim',[Ycent-40 Ycent+40]);
     caxis([minc maxc]);hold off;
-    
-    
-    %caxis([0 0.1]);
-    subplot(4,3,3);imagesc(MeanDiff{i});hold on;axis equal;axis tight;
-    plot(outT{i}{1}(:,2),outT{i}{1}(:,1),'-r');
-    plot(outI{i}{1}(:,2),outI{i}{1}(:,1),'-k');
-    set(gca,'XLim',[Tcent(i,1)-40 Tcent(i,1)+40]);
-    set(gca,'YLim',[Tcent(i,2)-40 Tcent(i,2)+40]);
-    
-    title('Ten. - PCAICA','FontSize',8);hold off;
+%     
+%     
+%     %caxis([0 0.1]);
+%     subplot(4,3,3);imagesc(MeanDiff{i});hold on;axis equal;axis tight;
+%     plot(outT{i}{1}(:,2),outT{i}{1}(:,1),'-r');
+%     plot(outI{i}{1}(:,2),outI{i}{1}(:,1),'-k');
+%     set(gca,'XLim',[Tcent(i,1)-40 Tcent(i,1)+40]);
+%     set(gca,'YLim',[Tcent(i,2)-40 Tcent(i,2)+40]);
+%     
+%     title('Ten. - PCAICA','FontSize',8);hold off;
     
     s(1) = subplot(4,3,4:6);
     
     temp = ICtrace(idx,:);
     temp = convtrim(temp,ones(1,10)./10);
-    plot(t,zscore(temp),'-k','DisplayName','IC trace','LineWidth',1);axis tight;hold on;
-    plot(t,zscore(trace(ROIidx(i),:)),'-r','LineWidth',1.5,'DisplayName','Raw T.trace');hold on;
-    gscore = max(trace(ROIidx(i),:))./std(trace(ROIidx(i),:));
+    %plot(t,zscore(temp),'-k','DisplayName','IC trace','LineWidth',1);axis tight;hold on;
+    plot(t,rawtrace(ROIidx(i),:),'-r','LineWidth',1.5,'DisplayName','Raw T.trace');axis tight;hold on;
+    gscore = max(rawtrace(ROIidx(i),:))./std(rawtrace(ROIidx(i),:));
     %set(gca,'YLim',[-3 15]);
     title(num2str(gscore));hold off;grid on;
     
@@ -72,11 +72,10 @@ for i = 1:length(NeuronImage)
     
     
     plot(t,FT(i,:),'-r','LineWidth',5,'DisplayName','T. activity');hold on;
-    plot(t,segFT(ROIidx(i),:),'-b','LineWidth',5,'DisplayName','T. seg activity');
-    plot(t,ICFT(idx,:),'-k','LineWidth',3,'DisplayName','ICA activity');axis tight;hold off;grid on;
-    
+    plot(t,segFT(ROIidx(i),:),'-b','LineWidth',5,'DisplayName','T. seg activity');axis tight;hold off;grid on;
+        
     s(3) = subplot(4,3,10:12);
-    plot(t,convtrim(CorrTrace(ROIidx(i),:),ones(1,10)./10),'-r','LineWidth',2,'DisplayName','corr');axis tight;hold off;
+    plot(t,fCorrTrace(ROIidx(i),:),'-r','LineWidth',2,'DisplayName','corr');axis tight;hold off;
     linkaxes(s,'x');hold off;grid on;
     pause;
     instr = 'y';
