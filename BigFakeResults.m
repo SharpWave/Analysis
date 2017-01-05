b4567289c0db02b4650962576a52a042935bbfd6
SharpWave <sullivan.dw@gmail.com>
2017-01-05 13:19:40 -0500

    changes

--- BigFakeResults.m
+++ BigFakeResults.m
@@ -1,24 +1,181 @@
 function [ output_args ] = BigFakeResults( input_args )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-
+close all;
 % make list of directories
 basedir = 'J:\PostSubTesting\Fake\';
 seeds = 1:5;
-sizes = [150,200,300];
-ICnums = [59,173,581]
+sizes = [150,200,300,400];
+ICnums = [59,173,581,1229]
 
 % iterate and load results
+% for i = 1:length(sizes)
+%     for j = 1:length(seeds)
+%         wdir = [basedir,'Fake',int2str(sizes(i)),'-',int2str(seeds(j))]
+%         cd(wdir)
+%         % load stuff here
+%         %MakeICoutput4(ICnums(i))
+%         %MakeFakeMovie(j,sizes(i));
+%         outs(i,j) = FakeDataResults;
+%     end
+% end
+% 
+% 
+% 
+% cd('C:\Users\Dave\Desktop\Revision\MatlabDumpingGround')
+% save outs.mat
+load outs.mat;
+
+% ugh, unpack the variables from the struct
 for i = 1:length(sizes)
     for j = 1:length(seeds)
-        wdir = [basedir,'Fake',int2str(sizes(i)),'-',int2str(seeds(j))];
-        cd(wdir);
-        % load stuff here
-        MakeICoutput4(ICnums(i))
+        TnspsROIFalsePos(i,j) = outs(i,j).TnspsROIFalsePos;
+        TnspsROIFalseNeg(i,j) = outs(i,j).TnspsROIFalseNeg;
+        PicaROIFalsePos(i,j) = outs(i,j).PicaROIFalsePos;
+        PicaROIFalseNeg(i,j) = outs(i,j).PicaROIFalseNeg;
+        
+        TnspsN(i,j) = outs(i,j).TnspsN;
+        PicaN(i,j) = outs(i,j).PicaN;
+        FakeN(i,j) = outs(i,j).FakeN;
+        
+        PicaFalseNegRate{i,j} = outs(i,j).PicaFalseNegRate;
+        TnspsFalseNegRate{i,j} = outs(i,j).TnspsFalseNegRate;
+        PicaFalsePosRate{i,j} = outs(i,j).PicaFalsePosRate;
+        TnspsFalsePosRate{i,j} = outs(i,j).TnspsFalsePosRate;
+        
+        NumTnspsTransients{i,j} = outs(i,j).NumTnspsTransients;
+        NumPicaTransients{i,j} = outs(i,j).NumPicaTransients;
+        NumFakeTransients{i,j} = outs(i,j).NumFakeTransients;
+        
+        PicaPartner{i,j} = outs(i,j).PicaPartner;
+        TnspsPartner{i,j} = outs(i,j).TnspsPartner;
     end
 end
 
+
+
 % tally results and plot
+
+%% figure 1: size estimates
+figure(1);hold on;
+plot(sizes.^2,mean((PicaN./FakeN)'),'r');errorbar(sizes.^2,mean((PicaN./FakeN)'),std((PicaN./FakeN)'),'r','LineWidth',2)
+plot(sizes.^2,mean((TnspsN./FakeN)'),'b');errorbar(sizes.^2,mean((TnspsN./FakeN)'),std((TnspsN./FakeN)'),'b','LineWidth',2)
+set(gca,'Box','off');
+%set(gca,'Ylim',[0 1.1]);
+set(gca,'XTick',sizes.^2);
+xtl{1} = '150 x 150';
+xtl{2} = '200 x 200';
+xtl{3} = '300 x 300';
+set(gca,'XTickLabel',xtl);
+set(gca,'XTickLabelRotation',45);
+xlabel('movie size (# of pixels)');
+ylabel('mean neuron count accuracy (fraction of actual)');
+set(gcf,'Position',[814   388   533   461]);
+set(gcf, 'PaperPositionMode', 'auto');
+saveas(gcf,'ROI_count_accuracy','tif');
+
+%% figure 2: activity estimates for fake ROIs with partners
+figure(2);hold on;
+
+for i = 1:length(sizes)
+    for j = 1:length(seeds)
+        GoodPicaROI{i,j} = PicaPartner{i,j}(PicaPartner{i,j} > 0);
+        GoodTnspsROI{i,j} = TnspsPartner{i,j}(TnspsPartner{i,j} > 0);
+        %keyboard;
+        PicaActivityAccuracy{i,j} = abs(NumPicaTransients{i,j}(GoodPicaROI{i,j})-NumFakeTransients{i,j}(GoodPicaROI{i,j}))./NumFakeTransients{i,j}(GoodPicaROI{i,j});
+        TnspsActivityAccuracy{i,j} = abs(NumTnspsTransients{i,j}(GoodTnspsROI{i,j})-NumFakeTransients{i,j}(GoodTnspsROI{i,j}))./NumFakeTransients{i,j}(GoodTnspsROI{i,j});
+        
+        PAA(i,j) = mean(PicaActivityAccuracy{i,j})*100;
+        TAA(i,j) = mean(TnspsActivityAccuracy{i,j})*100;
+        
+        PicaFalsePos(i,j) = mean(PicaFalsePosRate{i,j}(GoodPicaROI{i,j}));
+        PicaFalseNeg(i,j) = mean(PicaFalseNegRate{i,j}(GoodPicaROI{i,j}));
+        
+        TnspsFalsePos(i,j) = mean(TnspsFalsePosRate{i,j}(GoodTnspsROI{i,j}));
+        TnspsFalseNeg(i,j) = mean(TnspsFalseNegRate{i,j}(GoodTnspsROI{i,j}));
+    end
+end
+
+plot(sizes.^2,mean(PAA'),'r');errorbar(sizes.^2,mean(PAA'),std(PAA'),'r','LineWidth',2)
+plot(sizes.^2,mean(TAA'),'b');errorbar(sizes.^2,mean(TAA'),std(TAA'),'b','LineWidth',2)
+set(gca,'Box','off');
+set(gca,'Ylim',[0 40]);
+set(gca,'XTick',sizes.^2);
+xtl{1} = '150 x 150';
+xtl{2} = '200 x 200';
+xtl{3} = '300 x 300';
+set(gca,'XTickLabel',xtl);
+set(gca,'XTickLabelRotation',45);
+xlabel('movie size (# of pixels)');
+ylabel('# of transients detected: mean percent error)');
+set(gcf,'Position',[814   388   533   461]);
+set(gcf, 'PaperPositionMode', 'auto');
+saveas(gcf,'transient_count_accuracy','tif');
+
+%% figure 3 same shits but for false positives and negatives
+figure(3);hold on;
+
+
+plot(sizes.^2,mean(PicaFalseNeg'),'r');errorbar(sizes.^2,mean(PicaFalseNeg'),std(PicaFalseNeg'),'r','LineWidth',2);
+plot(sizes.^2,mean(TnspsFalseNeg'),'b');errorbar(sizes.^2,mean(TnspsFalseNeg'),std(TnspsFalseNeg'),'b','LineWidth',2);
+set(gca,'Box','off');
+set(gca,'Ylim',[0 1]);
+set(gca,'XTick',sizes.^2);
+xlabel('movie size (# of pixels)');
+ylabel('false negative rate');
+
+xtl{1} = '150 x 150';
+xtl{2} = '200 x 200';
+xtl{3} = '300 x 300';
+set(gca,'XTickLabel',xtl);
+set(gca,'XTickLabelRotation',45);
+set(gca,'XTickLabel',xtl);
+set(gca,'XTickLabelRotation',45);
+set(gcf,'Position',[814   388   533   461]);
+set(gcf, 'PaperPositionMode', 'auto');
+saveas(gcf,'false_negs','tif');
+
+%% figure 3 same shits but for false positives and negatives
+figure(4);hold on;
+
+
+plot(sizes.^2,mean(PicaFalsePos'),'r');errorbar(sizes.^2,mean(PicaFalsePos'),std(PicaFalsePos'),'r','LineWidth',2);
+plot(sizes.^2,mean(TnspsFalsePos'),'b');errorbar(sizes.^2,mean(TnspsFalsePos'),std(TnspsFalsePos'),'b','LineWidth',2);
+set(gca,'Box','off');
+set(gca,'Ylim',[0 1]);
+set(gca,'XTick',sizes.^2);
+xlabel('movie size (# of pixels)');
+ylabel('false positive rate');
+
+xtl{1} = '150 x 150';
+xtl{2} = '200 x 200';
+xtl{3} = '300 x 300';
+set(gca,'XTickLabel',xtl);
+set(gca,'XTickLabelRotation',45);
+set(gcf,'Position',[814   388   533   461]);
+set(gcf, 'PaperPositionMode', 'auto');
+saveas(gcf,'false_pos','tif');
+
+
+keyboard;
+
+
+
+% plot(x,mean(Tnsps'),'bo','MarkerFaceColor','b','MarkerSize',2);hold on;errorbar(x,mean(Tnsps'),std(Tnsps'),'b','LineWidth',1);
+% plot(x,mean(Pica'),'rs','MarkerFaceColor','r','MarkerSize',2);hold on;errorbar(x,mean(Pica'),std(Pica'),'r','LineWidth',1);
+% %set(gca,'Xlim',[150 400]);
+% set(gca,'Ylim',[0 180]);
+% set(gca,'XTick',x);
+% xtl{1} = '150 x 150';
+% xtl{2} = '200 x 200';
+% xtl{3} = '300 x 300';
+%
+% %set(gca,'XTickLabel',xtl);
+
+% xlabel('movie size (pixels)');
+% ylabel('run time (minutes)');
+% set(gca,'Box','off');
+
 
 end
 
--- FakeDataResults.m
+++ FakeDataResults.m
@@ -1,4 +1,4 @@
-function [ output_args ] = FakeDataResults()
+function [outs] = FakeDataResults()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 close all
@@ -18,28 +18,28 @@ blankframe = zeros(Xdim,Ydim,'single');
 %% Thing 1: Neuron Count estimations
 TnspsN = NumNeurons;
 PicaN = length(ICimage);
-FakeN = size(fakePSA,1);
+FakeN = size(FakePSA,1);
 
 %% Thing 2: Activity Estimations
 
 % Get # of Transients and save the epoch lists
 for i = 1:FakeN
-    FakeEpochs{i} = NP_FindSupraThresholdTransients(FakePSA(i,:),eps);
+    FakeEpochs{i} = NP_FindSupraThresholdEpochs(FakePSA(i,:),eps);
     NumFakeTransients(i) = size(FakeEpochs{i},1);
 end
 
 for i = 1:PicaN
-    PicaEpochs{i} = NP_FindSupraThresholdTransients(ICFT(i,:),eps);
+    PicaEpochs{i} = NP_FindSupraThresholdEpochs(ICFT(i,:),eps);
     NumPicaTransients(i) = size(PicaEpochs{i},1);
 end
 
 for i = 1:TnspsN
-    TnspsEpochs{i} = NP_FindSupraThresholdTransients(PSAbool(i,:),eps);
+    TnspsEpochs{i} = NP_FindSupraThresholdEpochs(PSAbool(i,:),eps);
     NumTnspsTransients(i) = size(TnspsEpochs{i},1);
 end
 
-%% Thing 3: Distance Metrics
-
+%% Thing 3: Spatial Distance Metrics
+disp('spatial distance');
 % Get distance between every Fake ROI and every Tenaspis and PCAICA ROI
 
 % get centroids
@@ -47,32 +47,179 @@ for i = 1:FakeN
     % Calculate Centroid
     temp = blankframe;
     temp(FakePixelIdxList{i}) = 1;
-    rp = regionprops(temp,4);
-    [FakeCent(i,1),FakeCent(i,2)] = rp{1}.Centroid;
+    
+    rp = regionprops(temp);
+    
+    FakeCent(i,:) = rp(1).Centroid;
 end
 
 for i = 1:PicaN
     % Calculate Centroid
-    [PicaCent(i,1),PicaCent(i,2)] = ICprops{i}.Centroid;  
+    PicaCent(i,:) = ICprops{i}.Centroid;
 end
 
 for i = 1:TnspsN
-    rp = regionprops(NeuronImage{i},4);
-    [TnspsCent(i,1),TnspsCent(i,2)] = rp{1}.Centroid;
+    rp = regionprops(NeuronImage{i});
+    TnspsCent(i,:) = rp(1).Centroid;
 end
 
-% calculate distances
+% calculate distances and overlaps
 for i = 1:FakeN
-    
     for j = 1:PicaN
         PicaDist(i,j) = pdist([FakeCent(i,:);PicaCent(j,:)]);
+        PicaOverlap(i,j) = length(intersect(FakePixelIdxList{i},find(ICimage{j})))/length(FakePixelIdxList{i});
     end
+    PicaNeighbors{i} = find(PicaOverlap(i,:) > 0);
     
     for j = 1:TnspsN
         TnspsDist(i,j) = pdist([FakeCent(i,:);TnspsCent(j,:)]);
+        TnspsOverlap(i,j) = length(intersect(FakePixelIdxList{i},find(NeuronImage{j})))/length(FakePixelIdxList{i});
+    end
+    TnspsNeighbors{i} = find(TnspsOverlap(i,:) > 0);
+end
+
+%% Thing 4: Temporal correlations (overlaps only)
+disp('temporal');
+p = ProgressBar(FakeN);
+for i = 1:FakeN
+    
+    for j = 1:length(PicaNeighbors{i})
+        PicaFalsePos(i,j) = 0;
+        PicaFalseNeg(i,j) = 0;
+        % compare with this guy
+        nidx = PicaNeighbors{i}(j);
+        for k = 1:size(FakeEpochs{i},1)
+            Matched = false;
+            for m = 1:size(PicaEpochs{nidx},1)
+                if(any(intersect(FakeEpochs{i}(k,1):FakeEpochs{i}(k,2),PicaEpochs{nidx}(m,1):PicaEpochs{nidx}(m,2))))
+                    Matched = true;
+                    break;
+                end
+            end
+            if (~Matched)
+                PicaFalseNeg(i,j) = PicaFalseNeg(i,j)+1;
+            end
+        end
+        
+        for m = 1:size(PicaEpochs{nidx},1)
+            Matched = false;
+            for k = 1:size(FakeEpochs{i},1)
+                if(any(intersect(FakeEpochs{i}(k,1):FakeEpochs{i}(k,2),PicaEpochs{nidx}(m,1):PicaEpochs{nidx}(m,2))))
+                    Matched = true;
+                    break;
+                end
+            end
+            if (~Matched)
+                PicaFalsePos(i,j) = PicaFalsePos(i,j)+1;
+            end
+        end
+    end
+    
+    for j = 1:length(TnspsNeighbors{i})
+        TnspsFalsePos(i,j) = 0;
+        TnspsFalseNeg(i,j) = 0;
+        % compare with this guy
+        nidx = TnspsNeighbors{i}(j);
+        for k = 1:size(FakeEpochs{i},1)
+            Matched = false;
+            for m = 1:size(TnspsEpochs{nidx},1)
+                if(any(intersect(FakeEpochs{i}(k,1):FakeEpochs{i}(k,2),TnspsEpochs{nidx}(m,1):TnspsEpochs{nidx}(m,2))))
+                    Matched = true;
+                    break;
+                end
+            end
+            if (~Matched)
+                TnspsFalseNeg(i,j) = TnspsFalseNeg(i,j)+1;
+            end
+        end
+        
+        for m = 1:size(TnspsEpochs{nidx},1)
+            Matched = false;
+            for k = 1:size(FakeEpochs{i},1)
+                if(any(intersect(FakeEpochs{i}(k,1):FakeEpochs{i}(k,2),TnspsEpochs{nidx}(m,1):TnspsEpochs{nidx}(m,2))))
+                    Matched = true;
+                    break;
+                end
+            end
+            if (~Matched)
+                TnspsFalsePos(i,j) = TnspsFalsePos(i,j)+1;
+            end
+        end
+    end
+    p.progress;
+end
+p.stop;
+PicaFalseNegRates = (PicaFalseNeg)./NumFakeTransients';
+TnspsFalseNegRates = (TnspsFalseNeg)./NumFakeTransients';
+PicaFalsePosRates = PicaFalsePos./NumFakeTransients';
+TnspsFalsePosRates = TnspsFalsePos./NumFakeTransients';
+
+%% Thing 4 - Determine whether detection techniques found the ROIs
+
+%Highest overlapping ROI with a false negative false of less than 80% is considered the partner
+
+PicaPartner = zeros(1,FakeN);
+TnspsPartner = zeros(1,FakeN);
+
+for i = 1:FakeN
+    if ~(isempty(PicaNeighbors{i}))
+        [PicaFalseNegRate(i),idx] = min(PicaFalseNegRates(i,1:length(PicaNeighbors{i})));
+        if (PicaFalseNegRate(i) < 0.8)
+            PicaPartner(i) = PicaNeighbors{i}(idx);
+        end
+        PicaFalsePosRate(i) = PicaFalsePosRates(i,idx);
+    else
+        PicaFalseNegRate(i) = 0;
+        PicaFalsePosRate(i) = 0;
+    end
+    
+    if (~isempty(TnspsNeighbors{i}))
+        [TnspsFalseNegRate(i),idx] = min(TnspsFalseNegRates(i,1:length(TnspsNeighbors{i})));
+        if (TnspsFalseNegRate(i) < 0.8)
+            TnspsPartner(i) = TnspsNeighbors{i}(idx);
+        end
+        TnspsFalsePosRate(i) = TnspsFalsePosRates(i,idx);
+    else
+        TnspsFalseNegRate(i) = 0;
+        TnspsFalsePosRate(i) = 0;
     end
 end
+
+% now calculate ROI-based false pos and neg
+TnspsROIFalsePos = 0;
+TnspsROIFalseNeg = 0;
+PicaROIFalsePos = 0;
+PicaROIFalseNeg = 0;
+
+for i = 1:FakeN
+    if (PicaPartner(i) == 0)
+        PicaROIFalseNeg = PicaROIFalseNeg+1;
+    end
     
+    if (TnspsPartner(i) == 0)
+        TnspsROIFalseNeg = TnspsROIFalseNeg+1;
+    end
+end
+
+for i = 1:PicaN
+    if(~any(ismember(i,PicaPartner)))
+        PicaROIFalsePos = PicaROIFalsePos+1;
+    end
+end
+
+for i = 1:TnspsN
+    if(~any(ismember(i,TnspsPartner)))
+        TnspsROIFalsePos = PicaROIFalsePos+1;
+    end
+end
+
+% throw the important shit in a struct
+outs = v2struct(PicaPartner,TnspsPartner,TnspsROIFalsePos,TnspsROIFalseNeg,PicaROIFalsePos,PicaROIFalseNeg,TnspsN,PicaN,FakeN,PicaFalseNegRate,TnspsFalseNegRate,TnspsFalsePosRate,PicaFalsePosRate,NumTnspsTransients,NumPicaTransients,NumFakeTransients);
+
+save FakeDataResults.mat;
+
+
+
 
 
 
